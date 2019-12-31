d7af4ed21d3f00f086bfd7de0ff11a6e3350d23c
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-12-30 21:20:33 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Snake
+
+Snake game built with Lua and the LÖVE 2D game framework
+[Download LÖVE](https://love2d.org/) or install with Homebrew:
+
+```
+brew install love
+```
---
+++ main.lua
@@ -0,0 +1,109 @@
+TILE_SIZE = 32
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+MAX_TILES_X = WINDOW_WIDTH / TILE_SIZE
+MAX_TILES_Y = math.floor(WINDOW_HEIGHT / TILE_SIZE) - 1
+
+TILE_EMPTY = 0
+TILE_SNAKE_HEAD = 1
+TILE_SNAKE_BODY = 2
+TILE_APPLE = 3
+
+-- time in seconds that the snake moves one tile
+SNAKE_SPEED = 0.5
+
+local tileGrid = {}
+
+local snakeX, snakeY = 1, 1
+local snakeMoving = 'right'
+local snakeTimer = 0
+
+function love.load()
+    love.window.setTitle('Snake')
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false
+    })
+    math.randomseed(os.time())
+
+    initializeGrid()
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+
+    if key == 'left' then
+        snakeMoving = 'left'
+    elseif key == 'right' then
+        snakeMoving = 'right'
+    elseif key == 'up' then
+        snakeMoving = 'up'
+    elseif key == 'down' then
+        snakeMoving = 'down'
+    end
+end
+
+function love.update(dt)
+    snakeTimer = snakeTimer + dt
+
+    if snakeTimer >= SNAKE_SPEED then
+        if snakeMoving == 'up' then
+            snakeY = snakeY - 1
+        elseif snakeMoving == 'down' then
+            snakeY = snakeY + 1
+        elseif snakeMoving == 'left' then
+            snakeX = snakeX -1
+        else
+            snakeX = snakeX + 1
+        end
+    end
+end
+
+function love.draw()
+    drawGrid()
+    drawSnake()
+end
+
+function drawGrid()
+    for y = 1, MAX_TILES_Y do
+        for x = 1, MAX_TILES_X do
+            if tileGrid[y][x] == TILE_EMPTY then
+
+                -- change the color to white for the grid
+                love.graphics.setColor(1, 1, 1, 1)
+                love.graphics.rectangle('line', (x - 1) * TILE_SIZE, (y - 1) * TILE_SIZE,
+                    TILE_SIZE, TILE_SIZE)
+
+            elseif tileGrid[y][x] == TILE_APPLE then
+
+                -- change the color to red for an apple
+                love.graphics.setColor(1, 0, 0, 1)
+                love.graphics.rectangle('fill', (x -1) * TILE_SIZE, (y - 1) * TILE_SIZE,
+                    TILE_SIZE, TILE_SIZE)
+            end
+        end
+    end
+end
+
+function drawSnake()
+    love.graphics.setColor(0, 1, 0, 1)
+    love.graphics.rectangle('fill', snakeX, snakeY, TILE_SIZE, TILE_SIZE)
+end
+
+function initializeGrid()
+    for y = 1, MAX_TILES_Y do
+
+        table.insert(tileGrid, {})
+
+        for x = 1, MAX_TILES_X do
+            table.insert(tileGrid[y], TILE_EMPTY)
+        end
+    end
+
+    local appleX, appleY = math.random(MAX_TILES_X), math.random(MAX_TILES_Y)
+
+    tileGrid[appleY][appleX] = TILE_APPLE
+end
+
